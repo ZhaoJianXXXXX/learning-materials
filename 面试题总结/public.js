1cc6bee06dedc97b3d37d8632105b1e1a9c4e208
zhaojian <244502720@qq.com>
2020-05-21 11:02:21 +0800

    fix:内容丰富

--- 面试题总结/public.js
+++ 面试题总结/public.js
@@ -330,4 +330,41 @@ input:-webkit-autofill, textarea:-webkit-autofill, select:-webkit-autofill {
 30.在线编辑网页（静态）
     document.body.contentEditable = 'true';
 
+31.进入全屏
+    function launchFullscreen(element) {
+        if (element.requestFullscreen) {
+            element.requestFullscreen();
+        }else if(element.mozRequestFullScreen) {
+            element.mozRequestFullScreen();
+        }else if(element.msRequestFullscreen) {
+            element.msRequestFullscreen();
+        }else if(element.webkitRequestFullscreen) {
+            element.webkitRequestFullScreen();
+        }
+    }
+    launchFullscreen(document.documentElement);
+    launchFullscreen(document.getElementById("id")); //某个元素进入全屏
+
+32.退出全屏
+    function exitFullscreen() {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }else if(document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }else if(document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        }else if(document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        }
+    }
+    exitFullscreen();
+
+33.全屏事件
+    document.addEventListener("fullscreenchange", function (e) {
+        if (document.fullscreenElement) {
+            console.log("进入全屏");
+        }else{
+            console.log("退出全屏");
+        }
+    });
 
