4fe2e26f3823cefaf0f1688b51b7d76b77470b53
ZhaoJianXXXXX <38028723+ZhaoJianXXXXX@users.noreply.github.com>
2021-09-25 20:15:43 +0800

    Add files via upload
    feat: react演进

---
+++ react/React框架运行时优化方案的演进/计算fps.js
@@ -0,0 +1,36 @@
+var rAF = function () {
+    return (
+        window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        function (callback) {
+            window.setTimeout(callback, 1000 / 60);
+        }
+    );
+}();
+  
+var frame = 0;
+var allFrameCount = 0;
+var lastTime = Date.now();
+var lastFameTime = Date.now();
+  
+var loop = function () {
+    var now = Date.now();
+    var fs = (now - lastFameTime);
+    var fps = Math.round(1000 / fs);
+  
+    lastFameTime = now;
+    // 不置 0，在动画的开头及结尾记录此值的差值算出 FPS
+    allFrameCount++;
+    frame++;
+  
+    if (now > 1000 + lastTime) {
+        var fps = Math.round((frame * 1000) / (now - lastTime));
+        console.log(`${new Date()} 1S内 FPS：`, fps);
+        frame = 0;
+        lastTime = now;
+    };
+  
+    rAF(loop);
+}
+ 
+loop();
